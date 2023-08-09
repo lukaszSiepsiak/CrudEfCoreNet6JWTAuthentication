de36652f217ed68bb99cf446e572cd5886e40681
lsiepsiak <lsiepsiak@gmail.com>
2023-08-09 18:10:11 +0200

    First commit. Initial project

---
+++ CrudEfCoreNet6JWTAuthentication/Controllers/TeamController.cs
@@ -0,0 +1,81 @@
+﻿using CrudEfCoreNet6JWTAuthentication.Data;
+using CrudEfCoreNet6JWTAuthentication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudEfCoreNet6JWTAuthentication.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TeamController: ControllerBase
+{
+    private static AppDbContext _context;
+
+    public TeamController(AppDbContext context)
+    {
+        _context = context;
+    }
+  
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var teams = await _context.Teams.ToListAsync();
+        
+        return Ok(teams);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> Get(int id)
+    {
+        var team = await _context.Teams.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (team == null)
+            return BadRequest($"There is no team in this ID: {id}");
+
+        return Ok(team);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(Team team)
+    {
+        if (team == null)
+            return BadRequest("You cannot specify team");
+        
+        await _context.Teams.AddAsync(team);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("Get", team.Id, team);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Put(int id, string country)
+    {
+        var team = await _context.Teams.FirstOrDefaultAsync(team => team.Id == id);
+        
+        if (team == null)
+            return BadRequest($"Team with ID:{id} does not exists!");
+
+        if (String.IsNullOrWhiteSpace(country))
+            return BadRequest("You must correctly specify country property!");
+        
+        team.Country = country;
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var team = await _context.Teams.FirstOrDefaultAsync(team => team.Id == id);
+        
+        if (team == null)
+            return BadRequest($"Team with ID:{id} does not exists!");
+
+        _context.Teams.Remove(team);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+}
---
+++ CrudEfCoreNet6JWTAuthentication/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CrudEfCoreNet6JWTAuthentication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudEfCoreNet6JWTAuthentication.Data;
+
+public class AppDbContext: DbContext
+{
+    public DbSet<Team> Teams { get; set; }
+    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+}
